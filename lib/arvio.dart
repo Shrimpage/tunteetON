7c3f306c8108357b58b272da182990a50ae8e03f
Samppav <102037282+Samppav@users.noreply.github.com>
2024-02-15 21:52:40 +0200

    tunnetila valinta listwheel

--- lib/arvio.dart
+++ lib/arvio.dart
@@ -37,29 +37,88 @@ class Arvio extends StatelessWidget {
         ],
         ),
       ),
-      
       body: Center(
-        child: Container(
-          height: 400,
-          child: const ListWheelScrollViewX(itemExtent: 50,
-          scrollDirection: Axis.horizontal,
-          children: [ // Tähän buttonit jotka lähettää moodin databaseen
-            Text('1', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(width: 5),
-            Text('2', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(width: 5),
-            Text('3', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(width: 5),
-            Text('4', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(width: 5),
-            Text('5', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(width: 5),
-            Text('6', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-            SizedBox(width: 5),
-            Text('7', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),      
-          ])
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            const Text(
+              'Arvioi tunnetilasi: ',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 49, 54, 56)),
+              ),
+        const SizedBox(height: 200),
+        Container(
+          height: 200,        
+          child: ListWheelScrollViewX(
+            itemExtent: 150,
+            scrollDirection: Axis.horizontal,
+            children: [
+              ElevatedButton(
+                onPressed:() {}, // Tänne moodin lisääminen tietokantaan
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: Colors.red,
+                ),
+                child: const Text('1', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 49, 54, 56)))
+              ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed:() {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: Colors.orange,
+                ),
+                child: const Text('2', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,  color: Color.fromARGB(255, 49, 54, 56)))
+                ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed:() {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: const Color.fromARGB(255, 144, 122, 96),
+                ),
+                child: const Text('3', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,  color: Color.fromARGB(255, 49, 54, 56)))
+                ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed:() {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: Colors.grey,
+                ),
+                child: const Text('4', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,  color: Color.fromARGB(255, 49, 54, 56)))
+                ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed:() {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: const Color.fromARGB(255, 158, 205, 162), 
+                ),
+                child: const Text('5', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,  color: Color.fromARGB(255, 49, 54, 56)))
+                ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed:() {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: Colors.lightGreen, 
+                ),
+                child: const Text('6', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,  color: Color.fromARGB(255, 49, 54, 56)))
+                ),
+              const SizedBox(width: 5),
+              ElevatedButton(
+                onPressed:() {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150,150),
+                  backgroundColor: Colors.green, 
+                ),
+                child: const Text('7', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold,  color: Color.fromARGB(255, 49, 54, 56)))
+                ),
+            ]),          
           ),
-      ),
+          ],
+        ), 
+      ),  
       backgroundColor: const Color.fromARGB(255, 244, 246, 248),
     );
   }
