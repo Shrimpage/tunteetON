48e089c1029d81d4a04a3683c5b2f68420171e28
Shrimpage <jphaapaniemi@gmail.com>
2024-02-20 19:02:32 +0200

    darkmode olemassaoleviin asetuksiin yms korjauksia

--- lib/asetukset.dart
+++ lib/asetukset.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Asetukset extends StatefulWidget {
-  const Asetukset({super.key});
+  const Asetukset({required Key key}) : super(key: key);
 
   @override
   _AsetuksetSivuState createState() => _AsetuksetSivuState();
@@ -10,6 +10,7 @@ class Asetukset extends StatefulWidget {
 
 class _AsetuksetSivuState extends State<Asetukset> {
   bool _darkMode = false;
+  int _moodsPerDay = 3; // Default moods per day
 
   @override
   void initState() {
@@ -21,18 +22,21 @@ class _AsetuksetSivuState extends State<Asetukset> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       _darkMode = prefs.getBool('darkMode') ?? false;
+      _moodsPerDay = prefs.getInt('moodsPerDay') ?? 3; // Default moods per day if not set
     });
   }
 
   _saveSettings() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('darkMode', _darkMode);
+    prefs.setInt('moodsPerDay', _moodsPerDay);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.arrow_back), color: _darkMode ? Colors.white : Colors.black ,onPressed: () => Navigator.pushNamed(context, '/home'),),
         title: Text(
           'Settings',
           style: TextStyle(color: _darkMode ? Colors.white : Colors.black), // Change title color based on dark mode
@@ -54,6 +58,46 @@ class _AsetuksetSivuState extends State<Asetukset> {
               });
               _saveSettings();
             },
+          ),
+          ListTile(
+            title: Text(
+              'Moods per day',
+              style: TextStyle(color: _darkMode ? Colors.white : Colors.black), // Change text color based on dark mode
+            ),
+            trailing: DropdownButton<int>(
+              value: _moodsPerDay,
+              dropdownColor: _darkMode ? Colors.grey[900] : Colors.white, // Change dropdown menu color based on dark mode
+              elevation: 8, // Increase elevation for a shadow effect
+              style: TextStyle(
+              color: _darkMode ? Colors.white : Colors.black,
+              fontSize: 20,
+              ),
+              underline: Container(
+              height: 2,
+              color: _darkMode ? Colors.white : Colors.black, // Customize the underline color
+              ),
+              icon: Icon(
+              Icons.arrow_drop_down, // Add a dropdown icon
+              color: _darkMode ? Colors.white : Colors.black,
+              ),
+              items: <int>[3, 5, 7].map((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text(
+                    '$value',
+                    style: TextStyle(
+                      color: _darkMode ? Colors.white : Colors.black, // Change dropdown item text color based on dark mode
+                      fontSize: 20,
+                  ),),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _moodsPerDay = value!.toInt();
+                });
+                _saveSettings();
+              },
+            ),
           ),
           // Add more settings widgets here as needed
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
         '/home': (context) => MyHome(),
         '/kaavio': (context) =>  Kaavio(),
         '/arvio': (context) => Arvio(),
-        '/asetukset': (context) => const Asetukset(),
+        '/asetukset': (context) => Asetukset(key: UniqueKey(),),
       },
     );
   }
