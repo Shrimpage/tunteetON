759cb1951cd19a37c49cc5742dab70d8308ba402
Shrimpage <jphaapaniemi@gmail.com>
2024-02-23 10:53:49 +0200

    testiint

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'arvio.dart';
 import 'asetukset.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'api/firebase_api.dart';
 
 
-
-void main() {
+void main() /*async*/{
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+  //await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
 
